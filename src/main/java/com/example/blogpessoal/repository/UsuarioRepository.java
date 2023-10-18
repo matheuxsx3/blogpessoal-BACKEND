f9c9375257d8f43b1b28239f71e4c6a1ce87f54f
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-10-18 16:54:11 -0300

    excluido a pasta project

--- src/main/java/com/example/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/example/blogpessoal/repository/UsuarioRepository.java
@@ -2,12 +2,8 @@ package com.example.blogpessoal.repository;
 
 import com.example.blogpessoal.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome")String nome);
-    public Optional<Usuario> findByUsuario(String usuario);
+    Optional<Usuario> findByUsuario(String usuario);
 }
